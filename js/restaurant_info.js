6218f40fa2ffd50f7e9fe6aa705d197bf65dbfde
Elisheva-Sosnovitch-Behar <esosno@gmail.com>
2018-07-26 09:49:28 +0300

    Adjust colours and media queries

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<pk.eyJ1IjoiZWxpc29zIiwiYSI6ImNqanNhZ3BtdjZpY2Qza256MTdoY2Ftd24ifQ.mxkhkOhC8lJ1PTXXYyndIQ>',
+        mapboxToken: 'pk.eyJ1IjoiZWxpc29zIiwiYSI6ImNqanNhZ3BtdjZpY2Qza256MTdoY2Ftd24ifQ.mxkhkOhC8lJ1PTXXYyndIQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
