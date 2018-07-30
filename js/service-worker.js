3e3d6f7487de86c991f5549f0cafbccad2cf7502
Elisheva-Sosnovitch-Behar <esosno@gmail.com>
2018-07-30 15:00:13 +0300

    Fixes to service worker file

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,30 +1,58 @@
-self.addEventListener('fetch', event =>
-    console.log(event.request);
-);
-
 var staticCacheName = 'restaurant-reviews-cache-v1';
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(staticCacheName).then(cache => {
       return cache.addAll([
-        '/',
-        '/js/dbhelper.js',
-        '/js/main.js',
-        '/js/restaurant_info.js',
-        '/js/register.js',
-        '/css/styles.css',
-        '/imgs/*',
-        '/data/restaurants.json',
-        '/restaurant.html',
-        '/index.html'
+        './',
+        './js/dbhelper.js',
+        './js/main.js',
+        './js/restaurant_info.js',
+        './js/register.js',
+        './css/styles.css',
+        './img/1.jpg',
+        './img/2.jpg',
+        './img/3.jpg',
+        './img/4.jpg',
+        './img/5.jpg',
+        './img/6.jpg',
+        './img/7.jpg',
+        './img/8.jpg',
+        './img/9.jpg',
+        './img/10.jpg',
+        './img/1-med-min.jpg',
+        './img/2-med-min.jpg',
+        './img/3-med-min.jpg',
+        './img/4-med-min.jpg',
+        './img/5-med-min.jpg',
+        './img/6-med-min.jpg',
+        './img/7-med-min.jpg',
+        './img/8-med-min.jpg',
+        './img/9-med-min.jpg',
+        './img/10-med-min.jpg',
+        './img/nav-small.jpg',
+        './data/restaurants.json',
+        './restaurant.html',
+        './index.html'
       ]);
     }).catch(error => {
         console.log("Caches open failed:" + error);
-    });
-  );
+    })
+  ); 
 });
 
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            if(response) {
+                return response;
+            }
+            return fetch(event.request);
+        }).catch(error => {
+            console.log("Cache retrieval failed:" + error);
+        })
+    );
+})
 
 //self.addEventListener('fetch', event => {
 //   var requestUrl = new URL(event.request.url);
@@ -43,8 +71,8 @@ self.addEventListener('install', event => {
 //  );
 //});
 
-self.addEventListener('message', function(event) {
-  if (event.data.action === 'skipWaiting') {
-    self.skipWaiting();
-  }
-});
+//self.addEventListener('message', function(event) {
+//  if (event.data.action === 'skipWaiting') {
+//    self.skipWaiting();
+//  }
+//});
